13fa6ee16b593d3e2c13c66ac7622de52d776a35
truestevie <23562462+truestevie@users.noreply.github.com>
2020-04-12 21:58:38 +0200

    Detect pulse with interrupt service routine

--- pulsie.ino
+++ pulsie.ino
@@ -1,23 +1,17 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include "config.h"
 
 const int LED_WIFI = 2;
 const int sensor_pin = 5; // D1 pin - Use external pull-up resistor
 bool previous_level = true;
-bool current_level = true;
+volatile bool current_level = true; //Value changes in interrupt service routine, hence volatile
 int total_n_pulses_detected = 0;
 int previous_n_pulses_detected = 0;
 const int debounce_time = DEBOUNCE_TIME;
-unsigned long end_debounce_timestamp;
-bool waiting_for_debounce = false;
+volatile unsigned long end_debounce_timestamp; //Value changes in interrupt service routine, hence volatile
+volatile bool waiting_for_debounce = false;    //Value changes in interrupt service routine, hence volatile
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(LED_WIFI, OUTPUT);
-}
-
-void loop()
+void ICACHE_RAM_ATTR handlePulse() //Interrupt service routine, hence add ICACHE_RAM_ATTR attribute
 {
   current_level = digitalRead(sensor_pin);
   if (!waiting_for_debounce)
@@ -28,11 +22,15 @@ void loop()
       waiting_for_debounce = true;
     }
   }
-  else
+}
+
+void followUpPulse()
+{
+  if (waiting_for_debounce)
   {
     if (end_debounce_timestamp < millis())
     {
-      // new_level = digitalRead(sensor_pin);
+      current_level = digitalRead(sensor_pin);
       waiting_for_debounce = false;
       if (current_level == LOW && previous_level == HIGH)
       {
@@ -41,11 +39,27 @@ void loop()
       previous_level = current_level;
     }
   }
+}
 
+void printNumberOfCountedPulses()
+{
   if (previous_n_pulses_detected != total_n_pulses_detected)
   {
     Serial.print("Total number of detected pulses: ");
     Serial.println(total_n_pulses_detected);
   }
   previous_n_pulses_detected = total_n_pulses_detected;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_WIFI, OUTPUT);
+  attachInterrupt(sensor_pin, handlePulse, CHANGE);
+}
+
+void loop()
+{
+  followUpPulse();
+  printNumberOfCountedPulses();
 }
